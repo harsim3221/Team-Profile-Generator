255925faf7c7e9ddcc1e0754f295440ceb903ad3
Harsimran Kaur <harsim3221@gmail.com>
2024-02-29 14:28:55 +0000

    Adding index.html

---
+++ starter/lib/Manager.js
@@ -0,0 +1,21 @@
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+const Employee = require('./Employee');
+
+// manager constructor extends employee constructor 
+class Manager extends Employee {
+    constructor (name, id, email, officeNumber) {
+        // calling employee constructor
+        super (name, id, email); 
+        
+        this.officeNumber = officeNumber; 
+    }
+
+    // override employee role to manager 
+    getRole () {
+        return "Manager";
+    }
+}
+
+// to be exported 
+module.exports = Manager; 
